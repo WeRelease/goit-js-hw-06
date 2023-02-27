9d4dd1b4d7af15fd9b8fcd3e050a7171ace8d1ca
Serhii Melnychenko <114669462+WeRelease@users.noreply.github.com>
2023-02-27 11:36:22 +0100

    finish task 09 08 07

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector(".login-form");
+console.log(formEl);
 
+const onFormSubmit = (event) => {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+  if (!email.value || !password.value) {
+    alert("Please fill in all the fields!");
+  } else {
+    const formData = {
+      [email.name]: email.value,
+      [password.name]: password.value,
+    };
+    console.log(formData);
+    event.currentTarget.reset();
+  }
+};
+
+formEl.addEventListener("submit", onFormSubmit);
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+const btnEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+const changeColorForBody = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+};
+
+btnEl.addEventListener("click", changeColorForBody);
